f2fabc8dfbf0273644b49de319b0e50c145d69b3
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-09-26 11:59:19 -0600

    test: added model testing for #adult_patients

--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -3,4 +3,8 @@ require 'rails_helper'
 RSpec.describe Patient do
   it { should have_many :doctors_patients }
   it { should have_many(:doctors).through(:doctors_patients) }
+
+  it '#adult_patients' do
+    #add orderly? to test only adding 18+ year olds and in alphabetical order
+  end
 end
