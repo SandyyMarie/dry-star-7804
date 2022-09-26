bd29c1470786113f85a2c0262107831dff1726f1
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-09-26 11:33:19 -0600

    feat: completed US2, figured out pathing and finding by find_by rather than find

--- app/controllers/doctors_patients_controller.rb
+++ app/controllers/doctors_patients_controller.rb
@@ -1,6 +1,9 @@
 class DoctorsPatientsController < ApplicationController
   def destroy
-    require 'pry'; binding.pry
-    @relationship = DoctorsPatient.find(params[:])
+    doctor = Doctor.find(params[:doctor_id])
+    relationship = DoctorsPatient.find_by(doctor_id: params[:doctor_id], patient_id: params[:patient_id])
+    relationship.destroy
+
+    redirect_to doctor_path(doctor)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :doctors, only: [:show] do
+  resources :doctors, only: [:show, :destroy] do
       # resources :doctors_patients, only: [:destroy]
-      delete ':id/doctors_patients/:id', to: 'doctors_patients#destory'
   end
-
-  # resources :doctors_patients, only: [:destroy] do
-
-  # end
-
+  delete '/doctors/:doctor_id/doctors_patients/:patient_id', to: 'doctors_patients#destroy'
+    
 
 end
