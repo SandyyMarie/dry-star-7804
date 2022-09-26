2e778fee6dae3e5bbd6ad8106eaef51b0d6a25c0
Sandy Griffin <Sandra.Griffin@colorado.edu>
2022-09-26 11:53:18 -0600

    feat: added patients lists, running out of time, need to make model to filter for adult patients only and organize alphabetically

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,8 @@
 class Patient < ApplicationRecord
   has_many :doctors_patients
   has_many :doctors, through: :doctors_patients
+
+  def self.adult_patients
+    require 'pry'; binding.pry
+  end
 end
